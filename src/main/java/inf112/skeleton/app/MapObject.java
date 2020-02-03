aab5a804a6d2d3e66668afe3d44d5b58900ac578
Erlend Dahl <erlend.h.dahl@gmail.com>
2020-02-03 22:51:33 +0100

    Object interfaces
    Added MapObject interface, DirectionalObject interface and Dir (direction) Enum

---
+++ src/main/java/inf112/skeleton/app/MapObject.java
@@ -0,0 +1,29 @@
+package inf112.skeleton.app;
+
+public interface MapObject {
+
+    /**
+     * Get X coordinate
+     */
+    public void getX();
+
+    /**
+     * Get Y coordinate
+     */
+    public void getY();
+
+    /**
+     * Set X coordinate
+     */
+    public void setX();
+
+    /**
+     * Set Y coordinate
+     */
+    public void setY();
+
+    /**
+     * Remove object from map
+     */
+    public void remove();
+}
