dada9b9e9f3bac9bdf61e9cbc60de6617bc2558a
Egil Bru <43035083+Egglis@users.noreply.github.com>
2020-02-04 14:16:27 +0100

    Board class for MapObjects

--- src/main/java/inf112/skeleton/app/MapObject.java
+++ src/main/java/inf112/skeleton/app/MapObject.java
@@ -2,28 +2,28 @@ package inf112.skeleton.app;
 
 public interface MapObject {
 
-	/**
-	 * Get X coordinate
-	 */
-	public int getX();
+    /**
+     * Get X coordinate
+     */
+    public int getX();
 
-	/**
-	 * Get Y coordinate
-	 */
-	public int getY();
+    /**
+     * Get Y coordinate
+     */
+    public int getY();
 
-	/**
-	 * Set X coordinate
-	 */
-	public void setX();
+    /**
+     * Set X coordinate
+     */
+    public void setX(int x);
 
-	/**
-	 * Set Y coordinate
-	 */
-	public void setY();
+    /**
+     * Set Y coordinate
+     */
+    public void setY(int y);
 
-	/**
-	 * Remove object from map
-	 */
-	public void remove();
+    /**
+     * Remove object from map
+     */
+    public void remove();
 }
