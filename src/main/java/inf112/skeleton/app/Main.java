952c28cbdca84d9b8488ddcb8fe38b45e56cf516
Terje Baklund <Terje.Baklund@student.uib.no>
2020-02-04 14:29:43 +0100

    Small main changes
    Changed resolution to match the board size, changed what class the main runs.

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -7,12 +7,12 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class Main {
     public static void main(String[] args) {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-        cfg.title = "hello-world";
-        cfg.width = 480;
-        cfg.height = 320;
+        cfg.title = "roborally";
+        cfg.width = 768;
+        cfg.height = 768;
 
 
-        new LwjglApplication(new HelloWorld(), cfg);
+        new LwjglApplication(new Map(), cfg);
 
     }
 }
