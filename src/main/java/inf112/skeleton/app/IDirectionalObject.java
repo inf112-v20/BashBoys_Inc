35ea548dc4e2ae99dfdf8d6f7da48fc9b1aa6901
Kap010 <Kap010@uib.no>
2020-02-04 16:01:42 +0100

    Added more tests; addet testRobot

--- src/main/java/inf112/skeleton/app/IDirectionalObject.java
+++ src/main/java/inf112/skeleton/app/IDirectionalObject.java
@@ -1,14 +1,24 @@
 package inf112.skeleton.app;
 
+import inf112.skeleton.app.enums.Direction;
+import inf112.skeleton.app.enums.LeftRight;
+
 public interface IDirectionalObject extends IMapObject {
 
     /**
      * Get direction
      */
-    public void getDir();
+    public Direction getDir();
 
     /**
-     * Set direction
+     * turn direction by 1
      */
-    public void setDir();
+    public void turn(LeftRight lr);
+    
+    /**
+     * 
+     * @param lr LEFT or RIGHT
+     * @param turnss times to turn
+     */
+    public void turn(LeftRight lr, int turnss);
 }
--- src/test/java/inf112/skeleton/app/AppTest.java
+++ src/test/java/inf112/skeleton/app/AppTest.java
@@ -1,6 +1,7 @@
 package inf112.skeleton.app;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
@@ -9,39 +10,62 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.TestInfo;
 
+import inf112.skeleton.app.enums.Direction;
+import inf112.skeleton.app.object.TestRobot;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest {
 
-	@BeforeAll
-	public static void first() {
-		System.out.println("A");//Placeholder
-	}
+    @BeforeAll
+    public static void first() {
+        System.out.println("A");// Placeholder
+    }
 
-	@BeforeEach
-	public void init(TestInfo testInfo) {
-		System.out.println("Start..." + testInfo.getDisplayName());
-	}
+    @BeforeEach
+    public void init(TestInfo testInfo) {
+        System.out.println("Start..." + testInfo.getDisplayName());
+    }
 
-	@AfterEach
-	public void tearDown(TestInfo testInfo) {
-		System.out.println("Finished..." + testInfo.getDisplayName());
-	}
+    @AfterEach
+    public void tearDown(TestInfo testInfo) {
+        System.out.println("Finished..." + testInfo.getDisplayName());
+    }
 
-	/**
-	 * Rigorous Test :-)
-	 */
-	@Test
-	@DisplayName("AssertTrue(true)")
-	public void shouldAnswerWithTrue() {
-		assertTrue(true);
-	}
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    @DisplayName("AssertTrue(true)")
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
 
-	@Test
-	@DisplayName("AssertFalse(false)")
-	public void shouldAnswerWithFalse() {
-		assertFalse(false);
-	}
+    @Test
+    @DisplayName("AssertFalse(false)")
+    public void shouldAnswerWithFalse() {
+        assertFalse(false);
+    }
+
+    @Test
+    @DisplayName("Direction test")
+    public void dirTest() {
+        TestRobot robot = new TestRobot();
+        assertEquals(robot.getDir(), Direction.NORTH);
+        robot.turn(Direction.SOUTH);
+        assertFalse(robot.getDir() == Direction.NORTH);
+        assertEquals(robot.getDir(), Direction.SOUTH);
+    }
+    
+    @Test
+    @DisplayName("Move test")
+    public void moveTest() {
+        TestRobot robot = new TestRobot();
+        assertEquals(robot.getX(), 0);
+        robot.turn(Direction.SOUTH);
+        assertFalse(robot.getDir() == Direction.NORTH);
+        assertEquals(robot.getDir(), Direction.SOUTH);
+    }
 
 }
