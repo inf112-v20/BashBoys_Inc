ed472800aaf0cd81996434f51b6eec05429b0013
Kap010 <Kap010@uib.no>
2020-02-04 13:06:35 +0100

    Added eclipce-files to gitIgnore; Added Junit to pom.xml; Improved TestSkeleton with Before/After-Each

--- src/test/java/inf112/skeleton/app/AppTest.java
+++ src/test/java/inf112/skeleton/app/AppTest.java
@@ -2,25 +2,44 @@ package inf112.skeleton.app;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.TestInfo;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
+    @BeforeEach
+    public void init(TestInfo testInfo) {
+    	System.out.println("Start..." + testInfo.getDisplayName());
+    }  
+    @AfterEach
+    public void tearDown(TestInfo testInfo) {
+    	System.out.println("Finished..." + testInfo.getDisplayName());
+    }
+    
+    
     /**
      * Rigorous Test :-)
      */
     @Test
+    @DisplayName("AssertTrue(true)")
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
     }
 
     @Test
+    @DisplayName("AssertFalse(false)")
     public void shouldAnswerWithFalse()
     {
         assertFalse( false );
     }
+    
+    
 }
