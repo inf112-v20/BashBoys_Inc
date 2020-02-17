90df8a76a25ffc1cef30d56bb04ff6fbf1598e35
Egil Bru <43035083+Egglis@users.noreply.github.com>
2020-02-17 19:53:01 +0100

    Deck class + DeckTests
    Includes a initialization method that generates all cards acording to the rulebook

---
+++ src/main/java/inf112/skeleton/app/Deck.java
@@ -0,0 +1,130 @@
+package inf112.skeleton.app;
+
+import inf112.skeleton.app.enums.LeftRight;
+import inf112.skeleton.app.object.MoveCard;
+import inf112.skeleton.app.object.RotateCard;
+import inf112.skeleton.app.object.iCard;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class Deck {
+    private LinkedList<iCard> cards = new LinkedList<>();
+    public int DECK_SIZE = 84;
+
+    public Deck(){
+        initializeDeck();
+        shuffleDeck();
+    }
+
+    /**
+     * Initializes a normal deck of robo-rally cards
+     */
+    private void initializeDeck(){
+        for(int i = 1; i <= DECK_SIZE; i++){
+            if(i <= 6){
+                cards.push(new RotateCard(LeftRight.LEFT,i*10,true,"U-turn"));
+            } else if (i <= 42){
+                if(i % 2 == 0) { // Right Rotation
+                    cards.push(new RotateCard(LeftRight.RIGHT,i*10,false,"Rotate Right"));
+                } else {        // Left Rotation
+                    cards.push(new RotateCard(LeftRight.LEFT,i*10,false,"Rotate Left"));
+                }
+            } else if (i <= 48) {
+                cards.push(new MoveCard(-1,i*10,"Back Up"));
+            } else if (i <= 66) {
+                cards.push(new MoveCard(1,i*10,"Move 1"));
+            } else if (i <= 78) {
+                cards.push(new MoveCard(2,i*10,"Move 2"));
+            } else {
+                cards.push(new MoveCard(3,i*10,"Move 3"));
+            }
+        }
+        // Priority: 10 -> 60 | 6: U turn cards
+        // Priority: 70 -> 420 | 18: Left turn | 18: Right turn
+        // Priority: 430 -> 480 | 6: Back up cards
+        // Priority: 490 -> 660 | 18: Move 1
+        // Priority: 670 -> 780 | 12: Move 2
+        // Priority: 790 -> 840 | 6: Move 3
+        /* Option cards:
+           Ablative Coat, Abort Switch, Brakes, Circuit Breaker, Conditional Program, Crab Legs
+           Double-Barreled Laser, Dual Processor, Extra Memory, Fire Control, Flywheel, Fourth Gear
+           Gyroscopic Stabilizer, High-Powered Laser, Mechanical Arm, Mini Howitzer, Power-Down Shield
+           Pressor Beam, Radio Control, Ramming Gear, Rear-Firing Laser, Recompile, Reverse Gear
+           Scrambler, Superior Archive, Tractor Beam
+         */
+    }
+
+    /**
+     * Shuffles the current deck
+     */
+    public void shuffleDeck(){
+        Collections.shuffle(cards);
+    }
+
+    /**
+     * Resets the current deck into re-shuffled re-created deck
+     */
+    public void resetDeck(){
+        initializeDeck();
+        shuffleDeck();
+    }
+
+    /**
+     * Return the first "amount" cards in deck and remove it from deck
+     * @param amount
+     * @return ArrayList of all drawn cards
+     */
+    public ArrayList<iCard> getCards(int amount){
+        if(amount > cards.size()){
+            amount = cards.size();
+        }
+
+        ArrayList<iCard> temp = new ArrayList<>();
+        for(int i = 0; i < amount; i++){
+            temp.add(cards.poll());
+        }
+        return temp;
+    }
+
+    /**
+     * Look at all cards currently in the deck, don't delete them
+     * @return LinkedList of all cards
+     */
+    public LinkedList<iCard> peekAllCards(){
+        return cards;
+    }
+
+    /**
+     * Add a single card at the bottom of the deck
+     * @param card
+     */
+    public void addCard(iCard card){
+        if(getCurrentDeckSize() < DECK_SIZE){
+            cards.push(card);
+        } else {
+            throw new IllegalArgumentException("Can't add more cards to deck");
+        }
+
+    }
+
+    /**
+     * Add a list of cards at the bottom of the deck
+     * @param newCards
+     */
+    public void addListOfCards(ArrayList<iCard> newCards){
+        for(iCard card : newCards){
+            addCard(card);
+        }
+    }
+
+    /**
+     * Return the current deck size
+     * @return int
+     */
+    public int getCurrentDeckSize(){
+        return cards.size();
+    }
+
+}
---
+++ src/test/java/inf112/skeleton/app/DeckTest.java
@@ -0,0 +1,82 @@
+package inf112.skeleton.app;
+
+import inf112.skeleton.app.object.MoveCard;
+import inf112.skeleton.app.object.RotateCard;
+import inf112.skeleton.app.object.iCard;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+class DeckTest {
+    private Deck testDeck;
+    private int DECK_SIZE;
+
+    @BeforeEach
+    public void setup(){
+        this.testDeck = new Deck();
+        this.DECK_SIZE = testDeck.DECK_SIZE;
+    }
+
+    @Test
+    @DisplayName("Get a cards from deck test")
+    public void getCardsFromDeckTest(){
+        ArrayList<iCard> retrievedCards = testDeck.getCards(9);
+        assert !retrievedCards.isEmpty();
+        assertEquals(9,retrievedCards.size());
+    }
+
+    @Test
+    @DisplayName("Get all cards from deck")
+    public void getAllCardsTest() {
+        LinkedList<iCard> retrievedCards = testDeck.peekAllCards();
+        assertEquals(DECK_SIZE,retrievedCards.size());
+    }
+
+    @Test
+    @DisplayName("Get more cards than deck size test")
+    public void getMoreCardsThanDeckSizeTest(){
+        ArrayList<iCard> retrievedCards = testDeck.getCards(100);
+        assertEquals(DECK_SIZE,retrievedCards.size());
+        assert testDeck.peekAllCards().isEmpty(); // Check if deck is empty after operation
+    }
+
+    @Test
+    @DisplayName("Add card to deck test")
+    public void addCardToDeckTest(){
+        testDeck.getCards(20); // Removes 20 cards
+        MoveCard test = new MoveCard(1,100,"TestCard");
+        testDeck.addCard(test); // Adds card at the bottom
+        assertEquals(testDeck.peekAllCards().get(0),test);
+    }
+
+    @Test
+    @DisplayName("Add card to full deck test")
+    public void addCardToFullDeckTest(){
+        try {
+            testDeck.addCard(new MoveCard(1,100,"TestCard"));
+        } catch (IllegalArgumentException e){
+            assert true;
+        }
+    }
+
+    @Test
+    @DisplayName("Test if the correct amount of each type of card")
+    public void testIfCorrectCardsAreInDeck(){
+        int rotateCards = 0, TARGET_ROTATE_CARDS = 42;
+        int moveCards = 0, TARGET_MOVE_CARDS = 42;
+        for(iCard card : testDeck.peekAllCards()){
+            if(card instanceof RotateCard){
+                rotateCards++;
+            } else if (card instanceof MoveCard){
+                moveCards++;
+            }
+        }
+        assertEquals(rotateCards,TARGET_ROTATE_CARDS);
+        assertEquals(moveCards,TARGET_MOVE_CARDS);
+    }
+}
