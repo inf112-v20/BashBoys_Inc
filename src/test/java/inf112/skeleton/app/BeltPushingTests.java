d26adadb1ad56c4100f012453df890c98ef33481
Kap010 <Kap010@uib.no>
2020-03-03 14:27:19 +0100

    BeltPushingTest

--- src/test/java/inf112/skeleton/app/BeltPushingTests.java
+++ src/test/java/inf112/skeleton/app/BeltPushingTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import inf112.skeleton.app.enums.Direction;
+import inf112.skeleton.app.enums.LeftRight;
 import inf112.skeleton.app.object.Belt;
 import inf112.skeleton.app.object.Robot;
 
@@ -24,17 +25,41 @@ class BeltPushingTests {
     }
     
     @Test
-    @DisplayName("Two belts pushing a robots")
-    void twoBeltsPushingSingleRobot() {
+    @DisplayName("Double belts pushing a robots")
+    void twoDoubleBeltsPushingSingleRobot() {
         Board board = new Board(12, 12);
-        board.addItem(new Belt(Direction.NORTH), 5, 5);
-        board.addItem(new Belt(Direction.NORTH), 5, 6);
+        board.addItem(new Belt(Direction.NORTH,2), 5, 5);
+        board.addItem(new Belt(Direction.NORTH,2), 5, 6);
         Robot r = new Robot("JLoh");
         board.addItem(r, 5, 5);
         board.moveBelts();
-        board.moveBelts();
-        board.moveBelts();
         assertEquals(r.getY(),7);
+    }
+    
+    @Test
+    @DisplayName("Single belt pushing robot onto double belt but not more")
+    void singleThenDoubleBelt() {
+        Board board = new Board(12, 12);
+        board.addItem(new Belt(Direction.NORTH), 5, 5);
+        board.addItem(new Belt(Direction.NORTH,2), 5, 6);
+        Robot r = new Robot("JLoh");
+        board.addItem(r, 5, 5);
+        board.moveBelts();
+        assertEquals(r.getY(),6);
+    }
+    
+    @Test
+    @DisplayName("Testing that corner turns robot")
+    void cornerBeltTurningRobot() {
+        Board board = new Board(12, 12);
+        board.addItem(new Belt(Direction.NORTH,2), 5, 5);
+        board.addItem(new Belt(Direction.EAST,2,LeftRight.RIGHT), 5, 6);
+        Robot r = new Robot("JLoh");
+        board.addItem(r, 5, 5);
+        board.moveBelts();
+        assertEquals(r.getY(),6);
+        assertEquals(r.getX(),6);
+        assertEquals(r.getDir(),Direction.WEST);
     }
 
 }
