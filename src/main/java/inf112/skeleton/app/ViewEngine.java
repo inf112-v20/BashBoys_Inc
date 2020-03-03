6c4a5d5c517e156e05ee14c63fcd7bf3cfb4e89e
Mathias Haugsbø <mathiash98@gmail.com>
2020-03-03 15:12:30 +0100

    The robotTileset is now inserted into a 2d hashMap to get different colors for each robot
    robotTiles.get("robot 1").get("RobotFaceNorth") to get tile

--- src/main/java/inf112/skeleton/app/ViewEngine.java
+++ src/main/java/inf112/skeleton/app/ViewEngine.java
@@ -3,6 +3,7 @@ package inf112.skeleton.app;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.maps.tiled.*;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -11,9 +12,12 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import inf112.skeleton.app.enums.Direction;
 import inf112.skeleton.app.object.IMapObject;
 import inf112.skeleton.app.object.Robot;
+import jdk.nashorn.internal.runtime.Undefined;
 
+import javax.naming.NameNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class ViewEngine extends com.badlogic.gdx.Game {
     private final Board board;
@@ -25,7 +29,7 @@ public class ViewEngine extends com.badlogic.gdx.Game {
     private OrthographicCamera camera;
 
     private HashMap<String, TiledMapTile> mapTiles = new HashMap<>();
-    private HashMap<String, TiledMapTile> robotTiles = new HashMap<>();
+    private HashMap<String, HashMap<String, TiledMapTile>> robotTiles = new HashMap<>();
     private TiledMapTileLayer boardLayer;
     private TiledMapTileLayer robotLayer;
 
@@ -68,9 +72,16 @@ public class ViewEngine extends com.badlogic.gdx.Game {
 
         TiledMapTileSet robotTileSet = map.getTileSets().getTileSet("RobotTileSet");
         for (TiledMapTile tile : robotTileSet) {
-            Object property = tile.getProperties().get("robot 1");
-            if (property != null) {
-                robotTiles.put((String) property, tile);
+            MapProperties properties = tile.getProperties();
+            Iterator<String> keys = properties.getKeys();
+            if (keys.hasNext()) {
+                String player = keys.next();
+                Object direction = properties.get(player);
+                if (!robotTiles.containsKey(player)) {
+                    robotTiles.put(player, new HashMap<>());
+                }
+
+                robotTiles.get(player).put((String) direction, tile);
             }
         }
     }
@@ -98,12 +109,17 @@ public class ViewEngine extends com.badlogic.gdx.Game {
                 if (gameObject instanceof Robot) {
                     TiledMapTileLayer.Cell cell = robotLayer.getCell(x, y);
                     // GameObject is a robot, get tile from robotTileSet and add to robotLayer
-                    TiledMapTile tile = getRobotTileByName(getStrRobot(((Robot) gameObject).getDir()));
-                    if (cell == null) {
-                        cell = new TiledMapTileLayer.Cell();
-                        robotLayer.setCell(x, y, cell);
+                    try {
+                        TiledMapTile tile = getRobotTileByName(gameObject.getName(), getStrRobot(((Robot) gameObject).getDir()));
+                        if (cell == null) {
+                            cell = new TiledMapTileLayer.Cell();
+                            robotLayer.setCell(x, y, cell);
+                        }
+                        cell.setTile(tile);
+                    } catch (NameNotFoundException error) {
+                        // Name or direction of robot was not found
+                        System.out.println("Name or direction of robot was not found. " + gameObject.getName() + " - " + ((Robot) gameObject).getDir());
                     }
-                    cell.setTile(tile);
                 } else {
                     // GameObject is not a robot do something else
                     TiledMapTileLayer.Cell cell = boardLayer.getCell(x, y);
@@ -111,9 +127,12 @@ public class ViewEngine extends com.badlogic.gdx.Game {
                         cell = new TiledMapTileLayer.Cell();
                         boardLayer.setCell(x, y, cell);
                     }
-
-                    TiledMapTile tile = getMapTileByName(gameObject.getName());
-                    cell.setTile(tile);
+                    try {
+                        TiledMapTile tile = getMapTileByName(gameObject.getName());
+                        cell.setTile(tile);
+                    } catch (NameNotFoundException error) {
+                        System.out.println("Could not find the mapTile with name " + gameObject.getName());
+                    }
                 }
             }
         }
@@ -147,39 +166,53 @@ public class ViewEngine extends com.badlogic.gdx.Game {
      * @param name name of tile
      * @return map tile
      */
-    private TiledMapTile getMapTileByName(String name) {
-        return mapTiles.get(name);
+    private TiledMapTile getMapTileByName(String name) throws NameNotFoundException {
+        try {
+            return mapTiles.get(name);
+        } catch (Exception error) {
+            throw new NameNotFoundException();
+        }
     }
 
     /**
      * Get robot tile by name
-     * @param name - name of robot tile
+     * @param name - name of robot. (robot 1, robot 2 etc)
+     * @param direction - Direction of robot. (RobotFaceNorth)
      * @return robot tile
      */
-    private TiledMapTile getRobotTileByName(String name) {
-        return robotTiles.get(name);
+    private TiledMapTile getRobotTileByName(String name, String direction) throws NameNotFoundException {
+        try {
+            return robotTiles.get(name).get(direction);
+        } catch (Exception error) {
+            throw new NameNotFoundException();
+        }
     }
 
     /**
      * Clear layer
      * @param layer - layer to clear
      */
-    public void clearLayer(TiledMapTileLayer layer) {
+    private void clearLayer(TiledMapTileLayer layer) {
         int height = layer.getHeight();
         int width = layer.getWidth();
-        TiledMapTile clearTile = getMapTileByName("clear");
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                // Not sure which method is best set cell to null or set Tile to clearTile:
-                //layer.setCell(x, y, null);
-                TiledMapTileLayer.Cell cell = layer.getCell(x, y);
-                if (cell == null) {
-                    layer.setCell(x,y, new TiledMapTileLayer.Cell());
-                    cell = layer.getCell(x, y);
+        try {
+            TiledMapTile clearTile = getMapTileByName("clear");
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    // Not sure which method is best set cell to null or set Tile to clearTile:
+                    //layer.setCell(x, y, null);
+                    TiledMapTileLayer.Cell cell = layer.getCell(x, y);
+                    if (cell == null) {
+                        layer.setCell(x,y, new TiledMapTileLayer.Cell());
+                        cell = layer.getCell(x, y);
+                    }
+                    cell.setTile(clearTile);
                 }
-                cell.setTile(clearTile);
             }
+        } catch (NameNotFoundException error) {
+            System.out.println("Could not find the clear tile in mapTiles.");
         }
+
     }
 
     /**
