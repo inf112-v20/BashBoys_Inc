c8f0744a641e7e6d298caf9da874353181fc6658
Acigez <Terje.baklund@gmail.com>
2020-03-03 19:23:02 +0100

    New Textures
    Belt textures, better wall textures and a hole texture added.

--- assets/maps/TileSet1.tsx
+++ assets/maps/TileSet1.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.2" name="MapTileSet" tilewidth="64" tileheight="64" tilecount="16" columns="4">
- <image source="TileSet1.png" width="256" height="256"/>
+<tileset version="1.2" tiledversion="1.3.2" name="MapTileSet" tilewidth="64" tileheight="64" tilecount="120" columns="8">
+ <image source="TileSet1.png" width="512" height="960"/>
  <tile id="0">
    <properties>
     <property name="clear tile" value="clear tile"/>
