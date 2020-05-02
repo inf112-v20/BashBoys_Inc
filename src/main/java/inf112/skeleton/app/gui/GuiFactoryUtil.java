17f3b495affc75bef24e243091aed6614503aad5
Acigez <Terje.baklund@gmail.com>
2020-05-02 20:07:28 +0200

    Fixed placement and scale of priority

--- src/main/java/inf112/skeleton/app/gui/GuiFactoryUtil.java
+++ src/main/java/inf112/skeleton/app/gui/GuiFactoryUtil.java
@@ -6,7 +6,6 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import inf112.skeleton.app.Board;
@@ -106,7 +105,6 @@ public class GuiFactoryUtil {
      * @return TextButton.TextButtonStyle Style
      */
     public static TextButton.TextButtonStyle getTextStyle(String path) {
-        Skin skin = new Skin(Gdx.files.internal("assets/gui/skin/CustomSkin.json"));
         TextureAtlas atlas = new TextureAtlas("assets/gui/skin/CustomSkin.atlas");
         BitmapFont font = new BitmapFont(Gdx.files.internal("assets/gui/skin/CustomFont20.fnt"), atlas.findRegion("CustomFont20"));
         TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
